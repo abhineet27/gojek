56d004d1d6bcbb18a75c25014f9ce5e0798e66bb
abhineet <abhineet.kumar@snapdeal.com>
2016-11-26 23:27:38 +0530

    added parent and child modules

---
+++ gojek/gojek-server/src/main/java/com/gojek/server/Application.java
@@ -0,0 +1,15 @@
+package com.gojek.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.gojek")
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
